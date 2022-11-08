249703fe076fba3dcad43675313cd439c8e05e5f
ashley-oh <90736023+ashley-oh@users.noreply.github.com>
2022-11-08 01:39:33 -0500

    Update download_data.sh

--- preprocess/download_data.sh
+++ preprocess/download_data.sh
@@ -5,12 +5,12 @@
 # download conceptnet 5.7
 wget https://s3.amazonaws.com/conceptnet/downloads/2019/edges/conceptnet-assertions-5.7.0.csv.gz -P ../data/kear/
 gzip -d ../data/kear/conceptnet-assertions-5.7.0.csv.gz
-python extract_english_cpnet.py
+python3 extract_english_cpnet.py
 # extract cpnet with most edges or only a few relations
-python extract_all.py
+python3 extract_all.py
 
 # download wiktionary
 wget https://kaikki.org/dictionary/English/all-non-inflected-senses/kaikki.org-dictionary-English-all-n-laGJUY -O ../data/kear/download-all-senses.json
-python construct_wikdict.py
+python3 construct_wikdict.py
 
 
