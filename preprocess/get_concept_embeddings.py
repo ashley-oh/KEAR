a3f0e6c992e74107826cfd96ce9c8379d848be20
ashley-oh <90736023+ashley-oh@users.noreply.github.com>
2022-11-10 13:42:16 -0500

    Update get_concept_embeddings.py

--- preprocess/get_concept_embeddings.py
+++ preprocess/get_concept_embeddings.py
@@ -2,17 +2,22 @@ import json
 from sentence_transformers import SentenceTransformer
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 conceptnet_path = "../data/kear/filtered_edges.json"
 
 with open(conceptnet_path) as f:
   x = json.load(f)
   
+count_dict = defaultdict(0)
 concept =[]
+
 for i in x:
-  #for j in x[i]:
-  #  concept.append(j)
+  for j in x[i]:
+    concept.append(j)
+    count_dict[j]+=1
   concept.append(i)
+  counts_dict[i]+=1
   
 concept = list(set(concept))
 
@@ -20,9 +25,12 @@ model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 embeddings = model.encode(concept)
 
-df = pd.DataFrame(list(zip(concept, embeddings.tolist())), columns = ["word", "emb"])
+counts = [count_dict[i] for i in concept]
 
-df.to_csv("concept_embeddings_filtered.csv", sep = "\t")
+df = pd.DataFrame(list(zip(concept, embeddings.tolist(), counts)), columns = ["word", "emb", "count"])
+
+df.to_csv("concept_embeddings_counts.csv", sep = "\t")
 
 #concept_embedding.csv - original
 #concept_embedding_filtered.csv -filtered to only include keys
+#"concept_embeddings_counts.csv" - original + counts of how many times word appears in conceptnet
