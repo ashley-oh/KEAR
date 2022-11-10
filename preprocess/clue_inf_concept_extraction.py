a4022e5117d7d304d82fd1c86bab7a6de3cbccf0
ashley-oh <90736023+ashley-oh@users.noreply.github.com>
2022-11-10 17:11:52 -0500

    Update clue_inf_concept_extraction.py

--- preprocess/clue_inf_concept_extraction.py
+++ preprocess/clue_inf_concept_extraction.py
@@ -6,7 +6,10 @@ import torch
 from tqdm import tqdm
 import ast
 import numpy as np
+import sys 
 
+graded_weights = bool(sys.argv[1])
+filepath = sys.argv[1]
 
 def stuff(x):
      for i in tqdm(x, total = len(x)):
@@ -16,18 +19,19 @@ def stuff(x):
              inf_emb = torch.from_numpy(np.reshape(model.encode(inference), (1,-1)))
              clue_cos = cos(clue_emb, emb) *weights
              inf_cos = cos(inf_emb, emb) *weights
-             i["inputs"]["clue_context"]=df["word"][torch.argmax(clue_cos, dim =0).item()]
-             i["targets"]["inference_context"]=df["word"][torch.argmax(inf_cos, dim =0).item()]
+             i["inputs"]["clue_context"]=df["word"][torch.topk(clue_cos, 5).indices.tolist()]
+             i["targets"]["inference_context"]=df["word"][torch.topk(clue_cos, 5).indices.tolist()]
 
 
 df = pd.read_csv("concept_embeddings_counts.csv", sep = "\t")          
 #df = df[df["count"]>1]
-emb = [ast.literal_eval(i) for i in df["emb"].to_list()]
+emb = [ast.literal_eval(i) for i in tqdm(df["emb"].to_list(), total = len(df["emb"].to_list())]
 emb = torch.tensor(emb)
 
-count_to_weight = {1:.97, 2:.98, 3:99 }
-weights = [count_to_weight[i] if i <4 else 1 for i in df["count"].tolist()]
-weights = torch.tensor(weights)
+if graded_weights:                                        
+     count_to_weight = {1:.97, 2:.98, 3:99 }
+     weights = [count_to_weight[i] if i <4 else 1 for i in df["count"].tolist()]
+     weights = torch.tensor(weights)
 
 train_path = "/capstone/sherlock_train_v1_1.json"
 val_path = "/capstone/sherlock_val_with_split_idxs_v1_1.json"
@@ -41,9 +45,13 @@ with open(train_path) as f:
 with open(val_path) as f:
     v = json.load(f)
 
-
-#stuff(t)
-#stuff(v)
+stuff(v)
+with open("val_"+filepath) as f:
+     json.dump(v, f)
+                                         
+stuff(t)
+with open("train_"+filepath) as f:
+     json.dump(v, f)
 
   
   
